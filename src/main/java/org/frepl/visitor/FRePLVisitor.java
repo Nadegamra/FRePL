e17a289206cb0e05485bb9ff7ff058f6c4649744
Karolis Žukauskas <karolis.zukaus@gmail.com>
2022-04-23 23:05:37 +0300

    Switched to java

---
+++ src/main/java/org/frepl/visitor/FRePLVisitor.java
@@ -0,0 +1,2 @@
+package org.frepl.visitor;public class FRePLVisitor {
+}
---
+++ target/generated-sources/antlr4/FRePLBaseVisitor.java
@@ -0,0 +1,27 @@
+// Generated from FRePL.g4 by ANTLR 4.9.3
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link FRePLVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class FRePLBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements FRePLVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitProgram(FRePLParser.ProgramContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitConstant(FRePLParser.ConstantContext ctx) { return visitChildren(ctx); }
+}
